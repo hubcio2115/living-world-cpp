6fda226138630b8be808e6b9704e04295dc54b6e
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-04-30 20:50:37 +0200

    refactor

--- main.cpp
+++ main.cpp
@@ -8,12 +8,12 @@ int main() {
     Position p2(1, 1);
     Position p3(-3, -5);
 
-     std::cout << p1.toString() << std::endl;
-     std::cout << p2.toString() << std::endl;
-     std::cout << p3.toString() << std::endl;
-     std::cout << p1.distance(p2) << std::endl;
-     p2.move(4, 6);
-     std::cout << p2.toString() << std::endl;
+//     std::cout << p1.toString() << std::endl;
+//     std::cout << p2.toString() << std::endl;
+//     std::cout << p3.toString() << std::endl;
+//     std::cout << p1.distance(p2) << std::endl;
+//     p2.move(4, 6);
+//     std::cout << p2.toString() << std::endl;
 
     // Plant & Animal
     auto grass = Organism::createOrganism('G', p2, nullptr);
@@ -33,20 +33,20 @@ int main() {
 
     // World test
     World world(6, 6);
-    Position posP1(4, 5);
-    auto grassW1 = Organism::createOrganism('G', posP1, &world);
-    Position posP2(5, 4);
-    auto dandelionW2 = Organism::createOrganism('D', posP2, &world);
+    Position pos1(4, 5);
+    auto grass1 = Organism::createOrganism('G', pos1, &world);
+    Position pos2(5, 4);
+    auto dandelion2 = Organism::createOrganism('D', pos2, &world);
 
-    Position posW2(3, 2);
-    auto sheepW1 = Organism::createOrganism('S', posW2, &world);
-    Position posW3(2, 3);
-    auto sheepW2 = Organism::createOrganism('S', posW3, &world);
+    Position pos3(3, 2);
+    auto sheep1 = Organism::createOrganism('S', pos3, &world);
+    Position pos4(2, 3);
+    auto sheep2 = Organism::createOrganism('S', pos4, &world);
 
-    world.addOrganism(grassW1);
-    world.addOrganism(dandelionW2);
-    world.addOrganism(sheepW1);
-    world.addOrganism(sheepW2);
+    world.addOrganism(grass1);
+    world.addOrganism(dandelion2);
+    world.addOrganism(sheep1);
+    world.addOrganism(sheep2);
 
     auto positions = world.getVectorOfFreePositionsAround(Position(5, 5));
 

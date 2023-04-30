b2196f5c8f28ed72101019e2578bb033b0bb8d57
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-04-30 20:50:06 +0200

    refactoring

--- src/plants/Plant.cpp
+++ src/plants/Plant.cpp
@@ -3,6 +3,4 @@
 Plant::Plant(const OrganismInitParams &organism, const Position position, World *world) : Organism(organism, position,
                                                                                                    world) {}
 
-void Plant::move(const int dx, const int dy) {
-    Organism::move(0, 0);
-}
+void Plant::move(const int dx, const int dy) {}
